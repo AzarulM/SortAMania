8371946b42f8402ecf1187e0d92c5110ec20ee14
AzarulM <43213109+AzarulM@users.noreply.github.com>
2018-11-30 11:17:11 -0500

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,22 @@
-
 public class Runner {
 
     public static void main(String[] args) {
 
+        SortCompetition team17 = new Team17SortCompetition();
+
+        int [] randIntsArr = randomIntsArr(10000);
+        String[] randStringsArr = randomStringArr(10000, 5);
+
+        System.out.println("Unsorted");
+        printArr(randIntArr);
+        
+        long time = System.currentTimeMillis();
+        int median = team17.challengeOne(randIntArr);
+        time = System.currentTimeMillis() - time;
+        System.out.println("Challenge One Time Taken: " + time * 0.001 + " Seconds");
+        System.out.println("Median equals: " + median);
+
+        System.out.println("Sorted");
+        printArr(randIntsArr);
     }
 }
