78645211b70722d4a3c07b2a0a68c51a1ca478b7
AzarulM <43213109+AzarulM@users.noreply.github.com>
2018-12-04 11:35:02 -0500

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -7,7 +7,7 @@ public class Runner {
         int [] randIntArr = SortingAlg.randIntArr(10000);
         String[]randomStringArr = SortingAlg.randomStringArr(10000,5);
 
-        System.out.println("Unsorted");
+        System.out.println("Not Sorted");
         SortingAlg.printArr(randIntArr);
         
         long time = System.currentTimeMillis();
@@ -18,5 +18,12 @@ public class Runner {
 
         System.out.println("Sorted");
         SortingAlg.printArr(randIntArr);
+
+        long time2 = System.currentTimeMillis();
+        String randString2 = SortingAlg.randomString(5);
+        int idx2 = team17.challengeTwo(randomStringArr, );
+        time2 = System.currentTimeMillis() - time2;
+        System.out.println("Challenge Two Time Taken: " + time2 * 0.001 + " Seconds");
+        System.out.println("Position of '" + randString2 + "' : " + idx2);
     }
 }
