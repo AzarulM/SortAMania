faeed36d1bdbc3dcc5870429bc47fc7ef1e365db
AzarulM <43213109+AzarulM@users.noreply.github.com>
2018-12-04 11:36:18 -0500

    Update Team17SortCompetition.java

--- src/Team17SortCompetition.java
+++ src/Team17SortCompetition.java
@@ -5,4 +5,30 @@ public class Team17SortCompetition extends SortCompetition
         SortingAlg.quicksort(arr, 0, arr.length - 1);
         return (SortingAlg.median(arr));
     }
+
+    public int challengeTwo(String[] arr, String query);
+    {
+        SortingAlg.quicksort(arr, 0, arr.length - 1);
+        return (SortingAlg.ArrIdx(arr, query));
+    }
+
+    public int challengeThree(int[] arr);
+    {
+
+    }
+
+    public int challengeFour(int[][] arr);
+    {
+
+    }
+
+    public int challengeFive(Comparable[] arr, Comparable query);
+    {
+
+    }
+
+    public String greeting();
+    {
+
+    }
 }
