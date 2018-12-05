8475aa3b2c73aa31b6383e3d0e7d62b2db01824f
AzarulM <43213109+AzarulM@users.noreply.github.com>
2018-12-05 11:50:52 -0500

    Update Team17SortCompetition.java

--- src/Team17SortCompetition.java
+++ src/Team17SortCompetition.java
@@ -14,7 +14,8 @@ public class Team17SortCompetition extends SortCompetition
 
     public int challengeThree(int[] arr);
     {
-
+        SortingAlg.quicksort(arr, 0, arr.length - 1);
+        return (SortingAlg.median(arr));
     }
 
     public int challengeFour(int[][] arr);
@@ -27,8 +28,9 @@ public class Team17SortCompetition extends SortCompetition
 
     }
 
-    public String greeting();
+    public String greeting()
     {
-
+        return "Hello everyone! This is Team 17 Sort-A-Mania." ;
     }
+
 }
