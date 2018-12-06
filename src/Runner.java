34f94afe74545d13de9696c04ac05e35ab4d0718
AzarulM <43213109+AzarulM@users.noreply.github.com>
2018-12-05 23:24:08 -0500

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -13,8 +13,8 @@ public class Runner {
         long time = System.currentTimeMillis();
         int median = team17.challengeOne(randIntArr);
         time = System.currentTimeMillis() - time;
-        System.out.println("Challenge One Time Taken: " + time * 0.001 + " Seconds");
-        System.out.println("Median equals: " + median);
+        System.out.println("Challenge One Time: " + time * 0.001 + " Seconds");
+        System.out.println("Median: " + median);
 
         System.out.println("Sorted");
         SortingAlg.printArr(randIntArr);
@@ -26,13 +26,24 @@ public class Runner {
         String randString2 = SortingAlg.randomString(5);
         int idx2 = team17.challengeTwo(randomStringArr, randString2);
         time2 = System.currentTimeMillis() - time2;
-        System.out.println("Challenge Two Time Taken: " + time2 * 0.001 + " Seconds");
+        System.out.println("Challenge Two Time: " + time2 * 0.001 + " Seconds");
         System.out.println("Position of '" + randString2 + "' : " + idx2);
 
 
         /********************************************************************/
-
-
+        
+        
+        System.out.println("Not Sorted");
+        SortingAlg.printArr(randIntArr);
+        
+        long time3 = System.currentTimeMillis();
+        int median3 = team17.challengeOne(randIntArr);
+        time3 = System.currentTimeMillis() - time3;
+        System.out.println("Challenge Three Time: " + time3 * 0.001 + " Seconds");
+        System.out.println("Median: " + median3);
+        
+        System.out.println("Sorted");
+        SortingAlg.printArr(randIntArr);
 
 
     }
