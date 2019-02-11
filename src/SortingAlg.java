790ddfec465219cce1a9916822d6ebc124dbefde
AzarulM <43213109+AzarulM@users.noreply.github.com>
2019-02-11 11:44:41 -0500

    Update SortingAlg.java

--- src/SortingAlg.java
+++ src/SortingAlg.java
@@ -1,153 +1,73 @@
-public class SortingAlg
-{
+/**
+ * Created by Teacher on 1/28/2019.
+ * simpleMerge takes in two sorted arrays of
+ * integers and merges them to return one sorted
+ * array of integers.
+ */
+public class SimpleMerge {
+    public static int[] simpleMerge(int[] arr1, int[] arr2) {
+        int x = arr1.length;
+        int y = arr2.length;
+        int[] mergeArr = new int[x + y];
+        for (int i = 0; i < x; i++) {
+            mergeArr[i] = arr1[i];
+        }
+        for (int i = arr1.length; i < x + y; i++) {
+            mergeArr[i] = arr2[i - x];
+        }
 
-    public static String[] randomStringArr(int num, int length)
-    {
-        String [] arr = new String [num];
-        while (num > 0)
-        {
-            int i = 0;
-            String s = "";
-            while (i < length)
+        for (int j = 0; j < mergeArr.length; j++) {
+            for (int k = j + 1; k < mergeArr.length; k++)
             {
-                char c = (char)((Math.random()*26)+97);
-                s = s + c;
-                i++;
-            }
-            num --;
-            arr[num] = s;
-        }
-        return arr;
-    }
-
-    public static int[] randIntArr(int count)
-    {
-        int[] arr = new int[count];
-        for(int i = 0; i < arr.length; i++)
-        {
-            arr[i] = (int)(Math.random()*10000);
-        }
-        return arr;
-    }
-
-    public static void swapInt(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    public static void swapString(String[] arr, int i, int j)
-    {
-        String temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    public static String randomString(int length)
-    {
-        String str = "";
-        int i = 0;
-        while (i < length)
-        {
-            char c = (char) ((Math.random()*26)+97);
-            str = str + c;
-            i++;
-        }
-        return str;
-    }
-    public static void quicksort(int[] arr, int left, int right)
-    {
-        if (left < right)
-        {
-            int pivot = partition(arr,left,right);
-
-            quicksort(arr,left,pivot - 1);
-            quicksort(arr,pivot + 1, right);
-        }
-    }
-
-    public static int partition(int[]arr, int left, int right)
-    {
-        int pivot = arr[right];
-        int i = left - 1;
-        for(int j = left; j < right; j++)
-        {
-            if(arr[j] < pivot) {
-                i++;
-                swapInt(arr, i, j);
+                if (mergeArr[j] < mergeArr[k])
+                {
+                    
+                }
             }
         }
-        swapInt(arr,i + 1, right);
-        return i + 1;
-    }
-
-    public static void quicksortString(String[] arr, int left, int right)
-    {
-        if (left < right)
-        {
-            int pivot = partition(arr,left,right);
-
-            quicksortString(arr,left,pivot - 1);
-            quicksortString(arr,pivot + 1, right);
-        }
-    }
-
-    public static int partition(String[]arr, int left, int right)
-    {
-        String pivot = arr[right];
-        int i = left - 1;
-        for(int j = left; j < right; j++)
-        {
-            if(arr[j].compareTo(pivot) <= 0) {
-                i++;
-                swapString(arr, i, j);
-            }
-        }
-        swapString(arr,i + 1, right);
-        return i + 1;
-    }
-
-    public static void printArr(int[] arr)
-    {
-        String str = "";
-        for (int i = 0; i < arr.length; i++)
-        {
-            str = str + arr[i] + " ";
-        }
-        System.out.println(str);
-    }
-
-    public static void printArr(String[] arr)
-    {
-        String str = "";
-        for (int i = 0; i < arr.length; i++)
-        {
-            str = str + arr[i] + " ";
-        }
-        System.out.println(str);
-    }
-
-    public static int median(int [] arr)
-    {
-        if (arr.length % 2 == 0)
-        {
-            int middleArr = (arr.length / 2) - 1;
-            return ((arr[middleArr] + arr[middleArr + 1]) / 2);
-        }
-        else
-        {
-            int middle = (arr.length/2) - 1;
-            return (arr[middle]);
-        }
-    }
-
-    public static int ArrIdx(String[] arr, String query)
-    {
-        for (int i = 0; i < arr.length - 1; i++)
-        {
-            if (arr[i].equals(query))
-                return i;
-        }
-        return -1;
+        return mergeArr;
     }
 }
-
+    /*public static void mergeSort(int[] arr){
+        int n = arr.length;
+        int[] temp = new int[n];
+        mergeSortHelper(arr,0,n-1,temp);
+    }
+    /*public static void mergeSortHelper(int[] arr, int left, int right, int[] temp){
+        if(left<right){
+            int mid = (left+right)/2;
+            mergeSortHelper(arr,left,mid,temp);
+            mergeSortHelper(arr,mid+1,right,temp);
+            merge(arr,left,mid,right,temp);
+        }
+    }
+    public static void merge(int[] arr, int left, int mid, int right, int[] temp){
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+        while (i <= mid && j <= right) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            }
+            else {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= mid){
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
+        while (j <= right){
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+        for (k = left; k <= right; k++){
+            arr[k] = temp[k];
+        }
+    }
+}*/
